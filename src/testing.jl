db3251b725fbf0264f8746257425489422b5d3ee
Florian Oswald <florian.oswald@gmail.com>
2018-09-25 15:43:59 +0200

    Had to use OrderedDict and not DataStructures!

--- src/testing.jl
+++ src/testing.jl
@@ -1,22 +1,20 @@
 module testing
-using OrderedCollections
-using FileIO
+	using OrderedCollections
+	using FileIO
 
-greet() = print("Hello World!")
+	struct Eval
+		x :: OrderedDict
+	end
 
-struct Eval
-	x :: OrderedDict
-end
+	function dosave()
+		e = Eval(OrderedDict(:a=>1,:b=>3))
+		FileIO.save("file.jld2","e",e)
+		return e
+	end
 
-function dosave()
-	e = Eval(OrderedDict(:a=>1,:b=>3))
-	FileIO.save("file.jld2","e",e)
-	return e
-end
-
-function doread()
-	e = FileIO.load("file.jld2","e")
-	return e
-end
+	function doread()
+		e = FileIO.load("file.jld2","e")
+		return e
+	end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,10 @@
 using Test
 using testing
+using OrderedCollections
 
 @testset "read?" begin
     e1 = testing.dosave()
     e2 = testing.doread()
-    @test e1 == e2
+    @test e1.x[:a] == e2.x[:a]
+    @test e1.x[:b] == e2.x[:b]
 end
